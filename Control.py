8dc1898df8ce97c87674180deea920a707b8f795
PAMELA OLIVEIRA SANTOS <pamela.osantos3@senacsp.edu.br>
2024-04-23 21:58:48 -0300

    primeiro commit

--- Control.py
+++ Control.py
@@ -5,6 +5,9 @@ class Control:
         self.opcao = -1
         self.exer = Exercicios()
 
+
+
+
     def coletar(self):
         self.exer.num1 = int(input("Informe o primeiro número: "))
         self.exer.num2 = int(input("Informe o segundo número: "))
@@ -16,6 +19,26 @@ class Control:
               "\n2. Subtrair"         +
               "\n3. Dividir"          +
               "\n4. Multiplicar"      +
+              "\n5. Exercicio 1"      +
+              "\n6. Exercicio 2"      +
+              "\n7. Exercicio 3"      +
+              "\n8. Exercicio 4"      +
+              "\n9. Exercicio 5"      +
+              "\n10. Exercicio 6"     +
+              "\n11. Exercicio 7"     +
+              "\n12. Exercicio 8"     +
+              "\n13. Exercicio 9"     +
+              "\n14. Exercicio 10"    +
+              "\n15. Exercicio 11"    +
+              "\n16. Exercicio 12"    +
+              "\n17. Exercicio 13"    +
+              "\n18. Exercicio 14"    +
+              "\n19. Exercicio 15"    +
+              "\n20. Exercicio 16"    +
+              "\n21. Exercicio 17"    +
+              "\n22. Exercicio 18"    +
+              "\n23. Exercicio 19"    +
+              "\n24. Exercicio 20"    +
               "\nEscolha uma das opções acima: "))
 
     def operacao(self):
@@ -35,5 +58,19 @@ class Control:
             elif (self.opcao == 4):
                 self.coletar()
                 print(self.exer.multiplicar())
+            elif (self.opcao == 5):
+                print(self.exer.ex1())
+            elif (self.opcao == 6):
+                print(self.exer.ex2())
+            elif (self.opcao == 7):
+                print(self.exer.ex3())
+            elif (self.opcao == 6):
+                print(self.exer.ex2())
+
+
             else:
                 print("Código escolhido não é válido!")
+
+
+
+
--- Exercicios.py
+++ Exercicios.py
@@ -4,6 +4,7 @@ class Exercicios:
         # Construtor
         self.num1 = 0
         self.num2 = 0
+        self.numRes = ""
 
     def somar(self):
        return f"{self.num1} + {self.num2} = {self.num1 + self.num2}"
@@ -18,4 +19,65 @@ class Exercicios:
             return f"{self.num1} / {self.num2} = {self.num1 / self.num2}"
 
     def multiplicar(self):
+
         return f"{self.num1} * {self.num2} = {self.num1 * self.num2}"
+
+
+    #Exercicio 1 - Faça um programa que imprima os números 1 a 10.
+    def ex1(self):
+        for i in range(1,11,1):
+            self.numRes += f"{i}",
+
+        return self.numRes
+
+
+    #Exercício 2 - Faça um programa que imprima os números pares de 1 ao 20.
+    def ex2(self):
+        for i in range(2,21,2):
+            self.numRes += f"{i}",
+
+        return self.numRes
+
+    #Exercício 3 - Faça um programa que calcule a soma dos números de 1 a 100.
+    def ex3(self):
+        for i in range(1,101,1):
+            self.num1 += i
+
+        return self.num1
+
+    #Exercicio 4- Faça um programa que imprima os multiplos de 5 de 1 a 50.
+    def ex4(self):
+        for i in range (1,51,1):
+            if(i % 5 == 0):
+                self.numRes += f"{i}",
+
+        return self.numRes
+
+    #Exercicio 5- Faça um programa que peça ao usuário um número e imprima se ele é Par ou Impar.
+    def ex5(self):
+        if (self.num1 % 2 ==0):
+            return "Par"
+        else:
+            return "Impar"
+    #Exercício 6-Faça um programa que peça ao usuário um numero e imprima se é positivo, negativo ou zero.
+    def ex6(self):
+        if(self.num1 > 0):
+            return "Positivo"
+        elif(self.num1< 0):
+            return"Negativo"
+        else:
+            return"zero"
+    #Exercicio Faca um programa que peça ao usuário um número   e imprima a tabuada desse número
+
+    def ex07(self):
+        self.numRes =""
+
+        for i in range (0,11,1):
+            self.numRes +=f"{self.num1} * {i} = {self.num1 * i}\n"
+
+        return f"tabuada do {self.num1}:\n{self.numRes}"
+    def ex08(self):
+        self.numRes=""
+
+
+
